a92ceb2a3c12f974cd26511c711e9ca6b200b228
Rafal Brzoska <rbrzoska@future-processing.com>
2021-06-07 08:19:50 +0200

    Initial app

---
+++ src/app/models/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  firstname: string;
+  lastname: string;
+  avatar: string;
+  username: string;
+  city: string;
+  country: string;
+  registered: number;
+}
